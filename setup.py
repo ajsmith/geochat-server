d22f339da3d5dea0790c34c996b8ac208a32c1fd
Alex Smith <alex.smith@redhat.com>
2016-01-16 23:54:28 -0500

    Fix package config.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ geochatd=geochat.cli:main
 setup(
     name='geochat',
     version='0',
+    packages=find_packages('src'),
     package_dir={'': 'src'},
     test_suite='geochat.tests.suite',
     entry_points=entry_points,
